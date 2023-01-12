b41f75e76076fe7db56cceab409cece33e49faef
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2023-01-11 20:21:09 -0500

    first draft

--- packages/connect-query/src/connect-query-key.ts
+++ packages/connect-query/src/connect-query-key.ts
@@ -24,16 +24,16 @@ import { disableQuery } from './utils';
 
 /**
  * TanStack Query requires query keys in order to decide when the query should automatically update.
- *
+ * 
  * `QueryKey`s in TanStack Query are usually arbitrary, but Connect-Query uses the approach of creating a query key that begins with the least specific information: the service's `typeName`, followed by the method name, and ending with the most specific information to identify a particular request: the input message itself.
- *
+ * 
  * For example, for a query key might look like this:
- *
+ * 
  * @example
  * [
- *     "buf.connect.demo.eliza.v1.ElizaService",
- *     "Say",
- *     { sentence: "hello there" },
+ *   "buf.connect.demo.eliza.v1.ElizaService",
+ *   "Say",
+ *   { sentence: "hello there" },
  * ]
  */
 export type ConnectQueryKey<I extends Message<I>> = [
@@ -43,7 +43,7 @@ export type ConnectQueryKey<I extends Message<I>> = [
 ];
 
 /**
- * In situations where you want to use partial matching for TanStack Query `queryKey`s
+ * This type is useful in situations where you want to use partial matching for TanStack Query `queryKey`s
  */
 export type ConnectPartialQueryKey = [
   serviceTypeName: string,
@@ -52,9 +52,9 @@ export type ConnectPartialQueryKey = [
 
 /**
  * TanStack Query requires query keys in order to decide when the query should automatically update.
- *
+ * 
  * In Connect-Query, much of this is handled automatically by this function.
- *
+ * 
  * @see ConnectQueryKey for information on the components of Connect-Query's keys.
  */
 export const makeConnectQueryKeyGetter =
