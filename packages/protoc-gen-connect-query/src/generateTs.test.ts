cf79f99cf44049f83bae6175dbe1de0bb0c35524
bufdev <bufdev-github@buf.build>
2023-01-13 12:48:58 -0500

    Cleanup

--- packages/protoc-gen-connect-query/src/generateTs.test.ts
+++ packages/protoc-gen-connect-query/src/generateTs.test.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022 Buf Technologies, Inc.
+// Copyright 2021-2023 Buf Technologies, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import { generate } from './jest/helpers';
 
 describe('generateDts', () => {
   const expected = (target: Target) => [
-    '// Copyright 2021-2022 Buf Technologies, Inc.',
+    '// Copyright 2021-2023 Buf Technologies, Inc.',
     '//',
     '// Licensed under the Apache License, Version 2.0 (the "License");',
     '// you may not use this file except in compliance with the License.',
