26c19512377646a351d4a1a18b9f4ae21d7f8355
Timo Stamm <ts@timostamm.de>
2024-09-26 15:03:17 +0200

    format
    Signed-off-by: Timo Stamm <ts@timostamm.de>

--- packages/connect-query/src/create-use-query-options.ts
+++ packages/connect-query/src/create-use-query-options.ts
@@ -91,10 +91,7 @@ export function createUseQueryOptions<
 >(
   methodSig: MethodUnaryDescriptor<I, O>,
   input: DisableQuery | PartialMessage<I> | undefined,
-  {
-    transport,
-    callOptions,
-  }: ConnectQueryOptions,
+  { transport, callOptions }: ConnectQueryOptions,
 ): {
   queryKey: ConnectQueryKey<I>;
   queryFn: QueryFunction<O, ConnectQueryKey<I>>;
