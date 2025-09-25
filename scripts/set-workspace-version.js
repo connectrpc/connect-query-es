73177a9fa8e08dc1d45541d5731c94a9be8b3aef
Paul Sachs <psachs@buf.build>
2025-09-25 10:04:13 -0400

    Updated semver version check with official regex
    Signed-off-by: Paul Sachs <psachs@buf.build>

--- scripts/set-workspace-version.js
+++ scripts/set-workspace-version.js
@@ -18,7 +18,14 @@
 import { readFileSync, writeFileSync, existsSync, globSync } from "node:fs";
 import { dirname, join } from "node:path";
 
-if (process.argv.length !== 3 || !/^\d+\.\d+\.\d+$/.test(process.argv[2])) {
+// Ensures that a valid semver version is provided
+// See https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+if (
+  process.argv.length !== 3 ||
+  !/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/.test(
+    process.argv[2],
+  )
+) {
   process.stderr.write(
     [
       `USAGE: ${process.argv[1]} <new-version>`,
